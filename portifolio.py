f7016c1b680c5053a3b2105a63f775e55fbb4ce1
Natan-Souz <natan-oliveira20111@hotmail.com>
2025-03-03 09:33:52 -0300

    refactor: refatorando Portfolio para o uso do banco de dados SQLite

--- portifolio.py
+++ portifolio.py
@@ -1,34 +1,30 @@
 from datetime import datetime
+from database import session, Transacao
 from data_handler import obter_preco_atual
 
 class Portfolio:
     def __init__(self):
         # Inicializa o portfólio carregando todas as transações do banco de dados
-        self.transacoes = [] #lista temporária
-
-    def carregar_transacoes(self):
-        #implementação posterior
-        pass    
+        self.transacoes = session.query(Transacao).all()
 
     def adicionar_transacoes(self, ticker, quantidade, preco, data, tipo):
-        transacao = {
-            "ticker": ticker,
-            "quantidade": quantidade,
-            "preco": preco,
-            "data": data,
-            "tipo": tipo,
-        }
+        transacao = Transacao(
+            ticker = ticker,
+            quantidade = quantidade,
+            preco = preco,
+            data = datetime.strptime(data, '%Y-%m-%d'),
+            tipo = tipo
+        )
+        session.add(transacao)
+        session.commit()
         self.transacoes.append(transacao)
-
-    def calcular_lucro_prejuizo(self,ticker):
-        #implementação posterior
-        pass
 
     def obter_posicao(self):
         posicao = {}
+
         for transacao in self.transacoes:
-            ticker = transacao["ticker"]
+            ticker = transacao.ticker
             if ticker not in posicao:
                 posicao[ticker] = 0
-            posicao[ticker] += transacao["quantidade"] if transacao["tipo"] == "compra" else - transacao["quantidade"]
+            posicao[ticker] += transacao.quantidade if transacao.tipo == 'compra' else -transacao.quantidade
         return posicao
