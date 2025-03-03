e33f16ac9b4bc04ebe02fd7d8435ae774dc963e9
Natan-Souz <natan-oliveira20111@hotmail.com>
2025-03-03 09:04:34 -0300

    feat: implementação temporaria do controle de portifolio sem banco de dados

--- portifolio.py
+++ portifolio.py
@@ -4,20 +4,31 @@ from data_handler import obter_preco_atual
 class Portfolio:
     def __init__(self):
         # Inicializa o portfólio carregando todas as transações do banco de dados
-        self.transacoes = []
+        self.transacoes = [] #lista temporária
 
     def carregar_transacoes(self):
         #implementação posterior
-        pass
+        pass    
 
     def adicionar_transacoes(self, ticker, quantidade, preco, data, tipo):
-        #implementação posterior
-        pass
+        transacao = {
+            "ticker": ticker,
+            "quantidade": quantidade,
+            "preco": preco,
+            "data": data,
+            "tipo": tipo,
+        }
+        self.transacoes.append(transacao)
 
     def calcular_lucro_prejuizo(self,ticker):
         #implementação posterior
         pass
 
     def obter_posicao(self):
-        #implementação posterior
-        pass
+        posicao = {}
+        for transacao in self.transacoes:
+            ticker = transacao["ticker"]
+            if ticker not in posicao:
+                posicao[ticker] = 0
+            posicao[ticker] += transacao["quantidade"] if transacao["tipo"] == "compra" else - transacao["quantidade"]
+        return posicao
