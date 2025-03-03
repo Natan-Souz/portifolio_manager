8a02944d76d5ce4712fe8b2f2585c9b7f219728d
Natan-Souz <natan-oliveira20111@hotmail.com>
2025-03-03 09:56:42 -0300

    fix: ajustando a instancia base do SQLAlchemy

--- database.py
+++ database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, Date
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 
+class Base(DeclarativeBase):
+    pass
+
 #conexão com SQLite
 engine = create_engine('sqlite:///portfolio.db')
-Base = DeclarativeBase()
 
 #Tabela de transações
 class Transacao(Base):
@@ -17,7 +19,7 @@ class Transacao(Base):
     tipo = Column(String) #Compra ou venda
 
 # Cria as tabelas no banco de dados
-Base.metadata.create_all(*engine)
+Base.metadata.create_all(engine)
 
 #sessão de interação com o db
 Session = sessionmaker(bind=engine)
